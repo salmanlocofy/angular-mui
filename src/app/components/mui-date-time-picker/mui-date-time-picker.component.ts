407faa729cdabf1e353fd71244e9df11fb4a443f
salmanlocofy <salman@locofy.ai>
2024-10-04 12:32:51 +0500

    mui fix

--- src/app/components/mui-date-time-picker/mui-date-time-picker.component.ts
+++ src/app/components/mui-date-time-picker/mui-date-time-picker.component.ts
@@ -1,9 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatDatepicker,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
+import {
+  MatNativeDateModule,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -19,16 +31,28 @@ import { FormsModule } from '@angular/forms';
     MatNativeDateModule,
     FormsModule,
   ],
+  providers: [provideNativeDateAdapter()],
 })
 export class MuiDateTimePickerComponent {
   @Input() placeholder: string = 'Select a date'; // Placeholder text
-  @Input() value: Date | null = null; // Selected date value
   @Input() disabled: boolean = false; // Disable the picker
   @Input() appearance: 'outline' | 'fill' = 'fill';
 
   @Output() valueChange = new EventEmitter<Date | null>(); // EventEmitter for date changes
+  value: Date | null = null; // Selected date value
 
-  onDateChange(event: any) {
-    this.valueChange.emit(event.value);
+  @ViewChild('picker') picker!: MatDatepicker<Date>; // Reference to the datepicker
+  // Handle date changes and close picker
+  onDateChange(event: any): void {
+    this.value = event.value;
+    this.valueChange.emit(this.value);
+    this.removeOverlay();
+  }
+
+  removeOverlay(): void {
+    const overlayContainer = document.querySelector('.cdk-overlay-container');
+    if (overlayContainer) {
+      overlayContainer.innerHTML = ''; // Remove all content within the overlay
+    }
   }
 }
